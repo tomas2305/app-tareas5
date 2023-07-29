e4f84eaf33bfeb0641429094d888b9a910dd6bed
tomas2305 <tomas23058@gmail.com>
2023-07-29 20:37:47 -0300

    deploy v6

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth'
+import { getAuth } from 'firebase/auth';
+import firebase_config from './firebase_config.json'
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: '${{secrets.FIREBASE_API_KEY}}',
-  authDomain: '${{secrets.FIREBASE_AUTH_DOMAIN}}',
-  databaseURL: '${{secrets.FIREBASE_DATABASE_URL}}',
-  projectId: 'app-tareas5',
-  storageBucket: '${{secrets.FIREBASE_STORAGE_BUCKET}}',
-  messagingSenderId: '${{secrets.FIREBASE_MESSAGING_SENDER_ID}}',
-  appId: '${{secrets.FIREBASE_APP_ID}}'
-};
-
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebase_config);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
