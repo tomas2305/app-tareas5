ee915991a0fa615108716efbfd9137b443100156
tomas2305 <tomas23058@gmail.com>
2023-07-29 20:08:05 -0300

    deploy v5

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,15 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  apiKey: '${{secrets.FIREBASE_API_KEY}}',
+  authDomain: '${{secrets.FIREBASE_AUTH_DOMAIN}}',
+  databaseURL: '${{secrets.FIREBASE_DATABASE_URL}}',
   projectId: 'app-tareas5',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  storageBucket: '${{secrets.FIREBASE_STORAGE_BUCKET}}',
+  messagingSenderId: '${{secrets.FIREBASE_MESSAGING_SENDER_ID}}',
+  appId: '${{secrets.FIREBASE_APP_ID}}'
 };
 
-console.log(process.env.FIREBASE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
